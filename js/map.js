2151a4474da794a367586734ac8cbdce6f98b2f9
Jedidiah Chibinga <108130890+jayd0421@users.noreply.github.com>
2026-01-20 12:48:59 +0100

    change no data popup text color

--- js/map.js
+++ js/map.js
@@ -69,7 +69,7 @@ function highlightFeature(e) {
             <strong
                 style="
                         font-size: 13px;
-                        color: red;
+                        color: rgb(27, 207, 27);
                         margin-bottom: 2px;
                         "
                     ;>
@@ -382,4 +382,5 @@ function suicidePopulationRatiosStyle(feature){
 // };
 
 // var layers = L.control.layers(baseLayers, overlays).addTo(map)  
+
 
